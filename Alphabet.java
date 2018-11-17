95c5f709356bc16a029a57a7cfae961b069df5f4
saranyasowndarajan <45116155+saranyasowndarajan@users.noreply.github.com>
2018-11-17 14:53:16 +0530

    Create Alphabet.java

---
+++ Alphabet.java
@@ -0,0 +1,11 @@
+class Alphabet
+{
+public static void main(String args[])
+{
+char a='*';
+if ((a>='a' && a<='z') || (a>='A' && a<='Z'))
+System.out.println("alphabet");
+else
+System.out.println("not an alphabet");
+}
+}
