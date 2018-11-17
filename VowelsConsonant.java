dcb761b1dc19a48d5a1be67d57ad8ac1f8745a7c
saranyasowndarajan <45116155+saranyasowndarajan@users.noreply.github.com>
2018-11-17 14:40:18 +0530

    Create VowelsConsonant.java

---
+++ VowelsConsonant.java
@@ -0,0 +1,17 @@
+class VowelsConsonant
+{
+public static void main(String args[])
+{
+char ch='a';
+siwtch(ch)
+{
+case 'a': System.out.println("a");
+case 'e': System.out.println("e");
+case 'i': System.out.println("i");
+case 'o': System.out.println("o");
+case 'u': System.out.println("u");
+default: 
+System.out.println("consonant");
+}
+}
+}
