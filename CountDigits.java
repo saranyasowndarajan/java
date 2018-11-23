09aee69da6eada432139fb8e8aa7a8918fd365dd
saranyasowndarajan <45116155+saranyasowndarajan@users.noreply.github.com>
2018-11-23 10:19:50 +0530

    Create CountDigits.java

---
+++ CountDigits.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.Scanner;
+class CountDigits {
+  public static void main(String[] args) {
+    int count;
+  Scanner in=new Scanner (System.in);
+  if (N<9 && N>-9)
+  
+     System.out.println(N);
+
+ while (N>0)
+  {
+  N=N/10;
+  count++;
+  }
+  System.out.println(count);
+}
+© 2018 GitHub, Inc.
