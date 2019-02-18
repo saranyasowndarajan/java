a638d4027c16b268dfd5785991dbdc67b4392719
saranyasowndarajan <45116155+saranyasowndarajan@users.noreply.github.com>
2019-02-18 10:07:35 +0530

    Create b17.c

---
+++ b17.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <math.h>
+ 
+void main()
+{
+    int number, sum = 0, rem = 0, cube = 0, temp;
+ 
+    printf ("enter a number");
+    scanf("%d", &number);
+    temp = number;
+    while (number != 0)
+    {
+        rem = number % 10;
+        cube = pow(rem, 3);
+        sum = sum + cube;
+        number = number / 10;
+    }
+    if (sum == temp)
+        printf ("yes");
+    else
+        printf ("no");
+}
