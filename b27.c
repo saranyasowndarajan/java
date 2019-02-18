e0ed0d57d07f1f21b992da06609e5d243e380624
saranyasowndarajan <45116155+saranyasowndarajan@users.noreply.github.com>
2019-02-18 11:52:52 +0530

    Create b27.c

---
+++ b27.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(void) {
+ char ch;
+int flag=0;
+scanf("%c",&ch);
+for(int i=48;i<=57;i++)
+{
+	if(ch==i) //Checking for ASCII value of entered character
+	{ flag=1; //If it is an Integer it must have ASCII value between 48-57
+		break;
+	}
+}
+if(flag==1)
+printf("Integer");
+else
+printf("Not Integer");
+  return 0;
+}
