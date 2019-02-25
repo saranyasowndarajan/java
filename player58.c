2a63a31d5280962c7954a81c6b923043a57cf437
saranyasowndarajan <45116155+saranyasowndarajan@users.noreply.github.com>
2019-02-25 11:29:46 +0530

    Create player58.c

---
+++ player58.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <string.h>
+int counting(char * str, char * sub);
+int main()
+{
+    char str[100];
+    char sub[10];
+    int count;
+    printf("Enter any string: ");
+    gets(str);
+    printf("Enter word to search occurrences: ");
+    gets(sub);
+    count = counting(str, sub);
+    printf("Total occurrences of '%s': %d", sub, count);
+    return 0;
+}
+int counting(char * str, char * sub)
+{
+    int i, j, flag, count;
+    int s1, s2;
+    s1 = strlen(str);     
+    s2 = strlen(sub);
+    count = 0;
+    for(i=0; i <= s1-s2; i++)
+    {
+        flag = 1;
+        for(j=0; j<searchLen; j++)
+        {
+            if(str[i + j] != sub[j])
+            {
+                flag = 0;
+                break;
+            }
+        }
+        if(flag == 1)
+        {
+            count++;
+        }
+    }
+    return count;
+}
