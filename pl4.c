31ff920f53c8b90fc9b7d487ddda6ad87079af1d
saranyasowndarajan <45116155+saranyasowndarajan@users.noreply.github.com>
2019-03-10 10:20:13 +0530

    Create pl4.c

---
+++ pl4.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<string.h>
+void main()
+{
+    char *s1,*s2;
+    scanf("%s",s1);
+    s2=".";
+    //s1=s1.append(s2);
+    printf("%s",s1);
+        printf("%s",s2);
+}
