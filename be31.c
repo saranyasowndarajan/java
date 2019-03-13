1fb3f77a37907b2770f9657a2dc75217f841959b
saranyasowndarajan <45116155+saranyasowndarajan@users.noreply.github.com>
2019-03-13 11:25:33 +0530

    Rename b31.c to be31.c

---
+++ be31.c
@@ -0,0 +1,26 @@
+/******************************************************************************
+
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+    char a[80];
+    int i, count=0;
+   gets(a);
+    for(i=0;i<strlen(a);i++)
+    {
+        if(a[i]!=' ')
+        {
+          count++;
+        }
+    }
+    printf("%d",count);
+    return 0;
+}
