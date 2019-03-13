c4a74a09c3c49a8fe1583c3fc0fb27c8075608a8
saranyasowndarajan <45116155+saranyasowndarajan@users.noreply.github.com>
2019-03-13 11:25:51 +0530

    Create be32.c

---
+++ be32.c
@@ -0,0 +1,26 @@
+/******************************************************************************
+
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+    char a[80];
+    int i, count=0;
+   gets(a);
+    for(i=0;i<strlen(a)+1;i++)
+    {
+        if(a[i]==' ' || a[i]=='\0')
+        {
+          count++;
+        }
+    }
+    printf("%d",count);
+    return 0;
+}
