9cd0e677018379f58e31b7e94965ea2b943e6627
saranyasowndarajan <45116155+saranyasowndarajan@users.noreply.github.com>
2019-03-13 15:04:18 +0530

    Update be35.c

--- be35.c
+++ be35.c
@@ -6,12 +6,9 @@ char a[100];
 int count=0;
    printf("Enter Paragraph : ");
    scanf("%s",a);
-  // printf("%llen",strlen(a));
     for(int i=0;i<strlen(a);i++)
     {
-        
-    //     printf("%c\n",a[i]);
-        if(a[i]>='0' && a[i]<='9')   
+      if(a[i]>='0' && a[i]<='9')   
         {
           count++;
         }
