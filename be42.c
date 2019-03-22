cf57bd29cb2214d638c02b3f40148d167078cd03
saranyasowndarajan <45116155+saranyasowndarajan@users.noreply.github.com>
2019-03-22 09:54:18 +0530

    Create be42.c

---
+++ be42.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<string.h>
+void main()
+{
+char a[100],b[100];
+int n;
+scanf("%s%s",a,b);
+int i=0,j=0;
+        if(a[i]>a[j])
+        {
+            printf("\n %s",a);;
+        }
+        else
+        printf("\n %s",b);
+}
