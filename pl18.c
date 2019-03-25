0f1413d61b038dafabc08efb662a0d3bddce30e7
saranyasowndarajan <45116155+saranyasowndarajan@users.noreply.github.com>
2019-03-25 16:05:43 +0530

    Update pl18.c

--- pl18.c
+++ pl18.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
- int check_anagram(char [], char []);
- int main()
+ 
+int check_anagram(char [], char []);
+ 
+int main()
 {
  char a[100][100], b[100];
- int n;
+ int n,count=0;
   printf("Enter a string\n");
   scanf("%d",&n);
   for(int i=0;i<n;i++)
@@ -15,30 +17,32 @@
   for(int i=0;i<n;i++)
   {
    if (check_anagram(a[i], b) == 1)
-    printf("The strings are anagrams.\n");
-  else
-    printf("The strings aren't anagrams.\n");
+   count++;
   }
+  printf("%d",count);
   return 0;
 }
- int check_anagram(char a[], char b[])
+ 
+int check_anagram(char a[], char b[])
 {
   int first[26] = {0}, second[26] = {0}, c=0;
-   while (a[c] != '\0')
+ 
+    while (a[c] != '\0')
   {
     first[a[c]-'a']++;
     c++;
   }
-  c = 0;
+   c = 0;
    while (b[c] != '\0')
   {
     second[b[c]-'a']++;
     c++;
   }
- for (c = 0; c < 26; c++)
+  for (c = 0; c < 26; c++)
   {
     if (first[c] != second[c])
       return 0;
   }
-   return 1;
+ 
+  return 1;
 }
