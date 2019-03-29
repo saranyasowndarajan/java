f4a0f911533714d2ddc19b2d4f8099ed13f04c0a
saranyasowndarajan <45116155+saranyasowndarajan@users.noreply.github.com>
2019-03-29 10:45:00 +0530

    Create be44.c

---
+++ be44.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<string.h>
+void main()
+{
+    int i,j=0,len,p;
+    scanf("%d",&p);
+        if(p>1 && p<10)
+        printf("yes");
+        else
+        printf("no");
+}
