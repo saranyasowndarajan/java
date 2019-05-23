69e3f4ed91d2de2659a3a5d4fa1ef7f17fbceecb
saranyasowndarajan <45116155+saranyasowndarajan@users.noreply.github.com>
2019-05-23 11:50:57 +0530

    Create be103.c

---
+++ be103.c
@@ -0,0 +1,33 @@
+
+#include <stdio.h>
+int main()
+{
+	char str[20]={0};	
+	int i;
+	scanf("%[^\n]s",str); 
+	for(i=0; str[i]!='\0'; i++)
+	{
+		if(i==0)
+		{
+			if((str[i]>='a' && str[i]<='z'))
+				str[i]=str[i]-32; 
+			continue; 
+		}
+		if(str[i]==' ')
+		{
+						++i;
+					if(str[i]>='a' && str[i]<='z')
+			{
+				str[i]=str[i]-32; 
+				continue; 
+			}
+		}
+		else
+		{
+						if(str[i]>='A' && str[i]<='Z')
+				str[i]=str[i]+32; 
+		}
+	}	
+	printf("Capitalize string is: %s\n",str);
+		return 0;
+}
