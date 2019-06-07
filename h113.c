c0c5845184ae34f19ef938d9477f95837e2fb38e
saranyasowndarajan <45116155+saranyasowndarajan@users.noreply.github.com>
2019-06-07 13:24:19 +0530

    Create h113.c

---
+++ h113.c
@@ -0,0 +1,27 @@
+/******************************************************************************
+
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <stdio.h>
+
+int main()
+{
+   int i,sum=0,j;
+   scanf("%d %d %d",&s,&e,&j);
+   for(i=s;i<j;i++)
+   {
+       if(sum<=e)
+       sum=sum+i;
+       p=sum;
+   }
+
+if(p==e)
+printf("yes");
+else
+printf("no");
+    return 0;
+}
