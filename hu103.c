180bed1e4cd27e1d6012b7c0373a8622f88df69b
saranyasowndarajan <45116155+saranyasowndarajan@users.noreply.github.com>
2019-06-14 12:03:27 +0530

    Rename h103.c to hu103.c

---
+++ hu103.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+void main()
+{
+   int i,j,rows;
+   printf("Input number of rows : ");
+   scanf("%d",&rows);
+    for(i=1;i<=rows;i++)
+   {
+	    for(j=1;j<=i;j++)
+	    {
+	    if(i==j)
+	     printf("1");
+	     else
+	     printf("1 \t 1 \t");
+	    }
+    	    printf("\n");
+   }
+}
