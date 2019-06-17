59a6151f0d7beb50bec4b5e47010968473803f77
saranyasowndarajan <45116155+saranyasowndarajan@users.noreply.github.com>
2019-06-17 15:15:42 +0530

    Create be93.c

---
+++ be93.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+float Celsius_to_Kelvin(float C) 
+{ 
+    return (C + 273.15); 
+} 
+ int main() 
+{ 
+       float C ; 
+  scanf("%f",&c);
+   printf("%f",Celsius_to_Kelvin(C));       
+    return 0; 
+} 
