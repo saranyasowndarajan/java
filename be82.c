78df0d6c2aa19f37190c283b3792e16d40c70e08
saranyasowndarajan <45116155+saranyasowndarajan@users.noreply.github.com>
2019-06-17 11:18:37 +0530

    Create be82.c

---
+++ be82.c
@@ -0,0 +1,11 @@
+int main()
+{
+	float length,breadth,area;
+	//printf("enter length of rectangle: ");
+	scanf("%f",&length);
+	//	printf("enter breadth of rectangle: ");
+	scanf("%f",&breadth);
+ 	area=(length*breadth);
+	printf("AOR: %.5f\n",area);
+	return 0;
+}
