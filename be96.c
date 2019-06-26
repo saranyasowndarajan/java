344c4a6a2b44de0ef33ebbcd686d4d9708a4273a
saranyasowndarajan <45116155+saranyasowndarajan@users.noreply.github.com>
2019-06-26 13:52:13 +0530

    Create be96.c

---
+++ be96.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<math.h>
+int main() {
+	int a,d,n,i,tn;
+	int sum=0;
+		scanf("%d",&a);
+		scanf("%d",&n);
+		scanf("%d",&d);
+	sum = ( n * ( 2 * a + ( n -1 ) * d ) )/ 2;
+	tn = a + (n-1) * d;
+		for (i=a;i<=tn; i= i + d ) {
+		if (i != tn)
+		             printf("%d + ",i); 
+                 else
+		             printf("%d = %d ",i,sum);
+	}
+	return 0;
+}
